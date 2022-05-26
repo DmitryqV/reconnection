5379b3eb2a71254118bdb3ad90011cdb52b33078
Dmitry Vlasenko <d.vlasenko@bit-tech.io>
2022-05-26 17:11:37 +0500

    fix nest scope, added guard scope

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,8 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+/**
+ * Guarding NESTFACTORY create SCOPED APP MODULE
+ * nf - NestFactory
+ **/
+(async (nf) => await (await nf.create(AppModule)).listen(2000))(NestFactory);

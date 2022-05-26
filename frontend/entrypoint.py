2b2ea7431f5f33c14f3c7b68cd6ed2c60ad3b960
Dmitry Vlasenko <d.vlasenko@bit-tech.io>
2022-05-26 19:01:03 +0500

    change code style

--- frontend/entrypoint.py
+++ frontend/entrypoint.py
@@ -4,7 +4,7 @@ from sys import argv
 launch__build__frontend  = "webpack --mode=production"
 launch__frontend         = "webpack serve"
 launch__prettier         = "yarn prettier --write ."
-launch__linter         = "yarn eslint '**/*.{ts,tsx}'"
+launch__linter           = "yarn eslint '**/*.{ts,tsx}'"
 
 if argv[1] == "frontend:start":
   os.system(f"{launch__prettier} && {launch__linter} && {launch__frontend}");

2e7b90644522945d20c7b00dd970fec17f22792e
Dmitry Vlasenko <d.vlasenko@bit-tech.io>
2022-05-26 14:56:03 +0500

    change port

--- frontend/sockets/socket.ts
+++ frontend/sockets/socket.ts
@@ -1,7 +1,7 @@
 import { io, Socket } from "socket.io-client";
 
 export const socket: Socket = io(
-  process.env.NODE_ENV === "production" ? "/" : "http://localhost:3001/",
+  process.env.NODE_ENV === "production" ? "/" : "http://localhost:3000/",
   {
     forceNew: true,
     timeout: 1000,

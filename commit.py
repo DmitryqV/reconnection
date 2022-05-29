c300eb8c9513f7e69218ac45fd8777cff704e5ea
Dmitry Vlasenko <d.vlasenko@bit-tech.io>
2022-05-29 15:14:10 +0500

    dmitry: change commit

--- commit.py
+++ commit.py
@@ -8,3 +8,11 @@ while len(__message__) == 0:
 
 os.system(f"git add . && git commit -a -m'{getpass.getuser()}: {__message__}' && git push")
 os.system("clear")
+
+__frontend__ = input("Start frontend ? y/n")
+
+while (__frontend__ != "y" | __frontend__ != "n"):
+  if __frontend__ == "y": 
+    os.system("yarn frontend")
+    os._exit(0)
+  if __frontend__ == "n": os._exit(0)

018b935a6f502a51384ebd1f073d81c42bf0a7dd
Dmitry Vlasenko <d.vlasenko@bit-tech.io>
2022-05-29 15:08:13 +0500

    dmitry: change commit py

--- commit.py
+++ commit.py
@@ -6,5 +6,6 @@ __message__ = ""
 while len(__message__) == 0:
   __message__ = input("Enter commit name: \n")
 
-os.system(f"git add . && git commit -a -m'{getpass.getuser()}: {__message__}' && git push");
-os.system(f"clear && exit")
+os.system(f"git add . && git commit -a -m'{getpass.getuser()}: {__message__}' && git push")
+os.system("clear")
+os.system("exit")

c04d0606fc810810b67de6db9b6e8a02fe3e6406
Dmitry Vlasenko <d.vlasenko@bit-tech.io>
2022-05-29 15:07:14 +0500

    dmitry: added clear function

--- commit.py
+++ commit.py
@@ -7,3 +7,4 @@ while len(__message__) == 0:
   __message__ = input("Enter commit name: \n")
 
 os.system(f"git add . && git commit -a -m'{getpass.getuser()}: {__message__}' && git push");
+os.system(f"clear")

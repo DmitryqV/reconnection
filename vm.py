b32cfecf33a9d54624e548526831df72672bd942
Dmitry Vlasenko <d.vlasenko@bit-tech.io>
2022-05-29 16:16:22 +0500

    added a script for connecting via ssh

---
+++ vm.py
@@ -0,0 +1,14 @@
+import os
+import re
+
+__VM__LIST__ = open('vm.list', 'r')
+__LIST__     = []
+
+count = 0
+
+for line in __VM__LIST__.readlines():
+    __LIST__.append(re.sub(r'\n', '', line))
+    print("{} connect: {}".format(count, line.strip()))
+    count += 1
+
+os.system(f"ssh {str(input('Send user name: '))}@{__LIST__[int(input('Send number: '))]}")

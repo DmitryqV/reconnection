3ce000b4f3c445f538a0834ac22a5611ce696d28
Dmitry Vlasenko <d.vlasenko@bit-tech.io>
2022-05-29 15:24:03 +0500

    remove useless func

--- commit.py
+++ commit.py
@@ -1,4 +1,3 @@
-import getpass
 import os
 
 __message__ = ""
@@ -6,7 +5,7 @@ __message__ = ""
 while len(__message__) == 0:
   __message__ = input("Enter commit name: \n")
 
-os.system(f"git add . && git commit -a -m'{getpass.getuser()}: {__message__}' && git push")
+os.system(f"git add . && git commit -a -m'{__message__}' && git push")
 os.system("clear")
 
 

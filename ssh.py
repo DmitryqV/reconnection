519ce34014ea806c0145af4554762b09ea841469
Dmitry Vlasenko <d.vlasenko@bit-tech.io>
2022-05-29 20:33:07 +0500

    refactoring ssh script

--- ssh.py
+++ ssh.py
@@ -9,8 +9,8 @@ __LIST__     = []
 count = 0
 
 for line in __VM__LIST__.readlines():
-    __LIST__.append(re.sub(r'\n', '', line))
-    print("{} connect: {}".format(count, line.strip()))
-    count += 1
+  print("{} connect: {}".format(count, line.strip()))
+  __LIST__.append(re.sub(r'\n', '', line))
+  count += 1
 
 os.system(f"ssh {str(input('Send user name: '))}@{__LIST__[int(input('Send number: '))]}")

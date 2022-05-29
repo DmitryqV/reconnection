bfb9964046f02cb364f63e8c0767dfa2f21c7db8
Dmitry Vlasenko <d.vlasenko@bit-tech.io>
2022-05-29 15:44:21 +0500

    added production scriptx

--- commit.py
+++ commit.py
@@ -5,7 +5,7 @@ __message__ = ""
 while len(__message__) == 0:
   __message__ = input("Enter commit name: \n")
 
-os.system(f"git add . && git commit -a -m'{__message__}' && git push")
+os.system(f"git add . && git commit -a -m'{__message__}x' && git push")
 os.system("clear")
 
 

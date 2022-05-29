9f162157fb20f38865b511470ac4e5c0ecc4cd8f
Dmitry Vlasenko <d.vlasenko@bit-tech.io>
2022-05-29 20:33:51 +0500

    remove useless scripts

--- commit.py
+++ commit.py
@@ -8,13 +8,5 @@ while len(__message__) == 0:
   __message__ = input("Enter commit name: \n")
 
 os.system(f"git add . && git commit -a -m'{__message__}' && git push")
+
 os.system("clear")
-
-
-__project__ = input("Start project ? fe/be : ")
-
-if __project__ == "fe": os.system("yarn frontend")
-
-if __project__ == "be": os.system("yarn backend")
-
-os._exit(0)

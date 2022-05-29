5ad109fb9e189d80e6b46ecdf112c3b9350ec2e2
Dmitry Vlasenko <d.vlasenko@bit-tech.io>
2022-05-29 15:25:14 +0500

    remove useless expression

--- commit.py
+++ commit.py
@@ -12,5 +12,7 @@ os.system("clear")
 __project__ = input("Start project ? fe/be : ")
 
 if __project__ == "fe": os.system("yarn frontend")
-elif __project__ == "be": os.system("yarn backend")
-else: os._exit(0)
+
+if __project__ == "be": os.system("yarn backend")
+
+os._exit(0)

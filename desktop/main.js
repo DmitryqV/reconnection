01617dce7ef1101ffdcd6df82775c03531021de6
Dmitry Vlasenko <d.vlasenko@bit-tech.io>
2022-05-30 14:05:20 +0500

    fix warnings and modules error

--- desktop/main.js
+++ desktop/main.js
@@ -1,15 +1,20 @@
 const { app, BrowserWindow } = require("electron");
 
+process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
+
 const createWindow = () => {
-  new BrowserWindow({
+  const win = new BrowserWindow({
     autoHideMenuBar: true,
-    // frame: false,
     height: 600,
     width: 800,
     webPreferences: {
       preload: require("path").join(__dirname, "preload.js"),
     },
-  }).loadFile("index.html");
+  });
+
+  win.loadFile("index.html");
+
+  process.env.NODE_ENV !== "production" && win.webContents.openDevTools();
 };
 
 app.on("window-all-closed", () => app.quit());

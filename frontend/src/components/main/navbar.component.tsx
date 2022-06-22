8f0cb0ebd961fccab6796a6bf66ae5ab5fc5d4a0
dmitryqv <vlasenko_dima@icloud.com>
2022-06-22 17:05:14 +0500

    added search params

--- frontend/src/components/main/navbar.component.tsx
+++ frontend/src/components/main/navbar.component.tsx
@@ -1,22 +1,29 @@
-import React, { FC } from "react";
+import React, { FC, ChangeEvent } from "react";
+import { useSearchParams } from "react-router-dom";
 
-export const NavbarComponent: FC = () => (
-  <div className="main__navbar">
-    <div className="main__navbar__search">
-      <input
-        type="text"
-        className="main__navbar__search-input"
-        placeholder="Поиск по интересам"
-      />
+export const NavbarComponent: FC = () => {
+  const [search, setSeatch] = useSearchParams();
+
+  return (
+    <div className="main__navbar">
+      <div className="main__navbar__search">
+        <input
+          onInput={(e: ChangeEvent<HTMLInputElement>) => setSeatch({auth: "developer", u: "developer", q: e.target.value, s: "development", socket: "socketID__development"})}
+          value={search.get("q")}
+          type="text"
+          className="main__navbar__search-input"
+          placeholder="Поиск по интересам"
+        />
+      </div>
+      <ul className="main__navbar__bookmarks">
+        <li className="main__navbar__badge">#GO</li>
+        <li className="main__navbar__badge">#JAVASCRIPT</li>
+        <li className="main__navbar__badge">#БИЗНЕС</li>
+        <li className="main__navbar__badge">#Программирование</li>
+        <li className="main__navbar__badge">#.NET</li>
+        <li className="main__navbar__badge">#твиты</li>
+        <li className="main__navbar__badge">#мемы</li>
+      </ul>
     </div>
-    <ul className="main__navbar__bookmarks">
-      <li className="main__navbar__badge">#GO</li>
-      <li className="main__navbar__badge">#JAVASCRIPT</li>
-      <li className="main__navbar__badge">#БИЗНЕС</li>
-      <li className="main__navbar__badge">#Программирование</li>
-      <li className="main__navbar__badge">#.NET</li>
-      <li className="main__navbar__badge">#твиты</li>
-      <li className="main__navbar__badge">#мемы</li>
-    </ul>
-  </div>
-);
+  )
+};

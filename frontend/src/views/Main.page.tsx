b5b757af849775cc59c28744f3b569410bf5f9d1
dmitryqv <vlasenko_dima@icloud.com>
2022-06-22 12:45:48 +0500

    create css & tsx files for main page

--- frontend/src/views/Main.page.tsx
+++ frontend/src/views/Main.page.tsx
@@ -1,3 +1,17 @@
 import React, { FC } from "react";
+import "./styles/Main.style.css";
 
-export const MainPage: FC = () => <>Main page</>;
+export const MainPage: FC = () => (
+  <section className="main__section">
+    <div className="main__navbar">
+      <div className="main__navbar__search">
+        <input type="text" className="main__navbar__search-input" />
+      </div>
+      <ul className="main__navbar__bookmarks">
+        <li className="main__navbar__badge">#GO</li>
+        <li className="main__navbar__badge">#JAVASCRIPT</li>
+        <li className="main__navbar__badge">#.NET</li>
+      </ul>
+    </div>
+  </section>
+);

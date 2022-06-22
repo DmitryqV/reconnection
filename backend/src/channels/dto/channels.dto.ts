20fa96c34ee8efdf6e2dcf1c943107da7bac0a85
dmitryqv <vlasenko_dima@icloud.com>
2022-06-22 20:50:37 +0500

    update channels dto

--- backend/src/channels/dto/channels.dto.ts
+++ backend/src/channels/dto/channels.dto.ts
@@ -2,9 +2,8 @@ export class createChannelDTO {
   readonly title: string
   readonly creator: string
   readonly limit: number
-  readonly peoples: number
   readonly price: number
   readonly banlist: string[]
-  readonly whitelist: string[]
+  readonly peopleslist: string[]
   readonly tags: string[]
 }

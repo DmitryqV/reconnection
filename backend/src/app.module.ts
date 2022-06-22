ff0fbd8d464506a8c3f01da4b5986cdafa74afdc
dmitryqv <vlasenko_dima@icloud.com>
2022-06-22 20:15:27 +0500

    update nest config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { ChannelsController } from './channels/channels.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ChannelsController],
   providers: [AppService],
 })
 export class AppModule {}

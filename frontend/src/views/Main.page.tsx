5b6741e5c9f91b67dce311d3814522302b7a9e04
dmitryqv <vlasenko_dima@icloud.com>
2022-06-22 13:03:13 +0500

    create navbar menu for main page

--- frontend/src/views/Main.page.tsx
+++ frontend/src/views/Main.page.tsx
@@ -5,12 +5,16 @@ export const MainPage: FC = () => (
   <section className="main__section">
     <div className="main__navbar">
       <div className="main__navbar__search">
-        <input type="text" className="main__navbar__search-input" />
+        <input type="text" className="main__navbar__search-input" placeholder="Поиск по интересам" />
       </div>
       <ul className="main__navbar__bookmarks">
         <li className="main__navbar__badge">#GO</li>
         <li className="main__navbar__badge">#JAVASCRIPT</li>
+        <li className="main__navbar__badge">#БИЗНЕС</li>
+        <li className="main__navbar__badge">#Программирование</li>
         <li className="main__navbar__badge">#.NET</li>
+        <li className="main__navbar__badge">#твиты</li>
+        <li className="main__navbar__badge">#мемы</li>
       </ul>
     </div>
   </section>

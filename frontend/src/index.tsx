25b50145009a91280d36413cd3a52b314900424c
dmitryqv <vlasenko_dima@icloud.com>
2022-06-22 12:34:15 +0500

    remove useless react dom element

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,5 @@
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import React, { StrictMode } from "react";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<StrictMode children={<App />}/>);
+createRoot(document.getElementById("root")).render(<StrictMode children={<App />}/>);

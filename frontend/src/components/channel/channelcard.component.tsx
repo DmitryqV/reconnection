1bfc017ec4eb3469d4e1017006d5edd1b9b471aa
dmitryqv <vlasenko_dima@icloud.com>
2022-06-22 16:46:25 +0500

    added card component

--- frontend/src/components/channel/channelcard.component.tsx
+++ frontend/src/components/channel/channelcard.component.tsx
@@ -3,27 +3,47 @@ import React, { FC } from "react";
 import bg from "../../assets/b.png";
 import star from "../../assets/star.svg";
 import person from "../../assets/person.svg";
+import ruble from "../../assets/ruble.svg";
 
-export const ChannelCardComponent:FC = () => (
-<li className="main__channel">
-  <img className="channel__preview" src={bg} />
-  <div className="channel__stats">
-    <div className="channel__stats__meta">
-      <span className="channel__star channel__meta">
-        4.3
-        <img src={star} />
-      </span>
-      <span className="channel__people channel__meta">
-        32/54
-        <img src={person} />
-      </span>
+interface IChannel {
+  price: number;
+  rating: number;
+  peoples: number;
+  limitPeoples: number;
+  title: string;
+  tags: string[];
+}
+
+export const ChannelCardComponent: FC<IChannel> = ({
+  price,
+  rating,
+  title,
+  tags,
+  peoples,
+  limitPeoples,
+}) => (
+  <li className="main__channel">
+    <img className="channel__preview" src={bg} />
+    <div className="channel__stats">
+      <div className="channel__stats__meta">
+        <span className="channel__star channel__meta">
+          {rating}
+          <img src={star} />
+        </span>
+        <span className="channel__people channel__meta">
+          {peoples}/{limitPeoples}
+          <img src={person} />
+        </span>
+        <span className="channel__isruble channel__meta">
+          {price} <img src={ruble} />
+        </span>
+      </div>
     </div>
-  </div>
-  <div className="channel__title">
-    Channel
-  </div>
-  <ul className="channel__tags">
-    <li className="channel__tag">#программирование</li>
-  </ul>
-</li>
+    <div className="channel__title">{title}</div>
+    <ul className="channel__tags">
+      {tags.map((tag: string) => (
+        <li className="channel__tag">#{tag}</li>
+      ))}
+    </ul>
+  </li>
 );
--- frontend/src/components/main/channels.component.tsx
+++ frontend/src/components/main/channels.component.tsx
@@ -9,13 +9,56 @@ export const ChannelComponent: FC<iChannel> = ({ title }) => (
   <section className="main__channels">
     <h1 className="main__channels__title">{title}</h1>
     <ul className="main__channels__content">
-      <ChannelCardComponent />
-      <ChannelCardComponent />
-      <ChannelCardComponent />
-      <ChannelCardComponent />
-      <ChannelCardComponent />
-      <ChannelCardComponent />
-      <ChannelCardComponent />
+      <ChannelCardComponent
+        {...{
+          title: "hello",
+          rating: 3.3,
+          price: 20,
+          peoples: 20,
+          limitPeoples: 53,
+          tags: ["dell", "js", "svg"],
+        }}
+      />
+      <ChannelCardComponent
+        {...{
+          title: "hello",
+          rating: 4.3,
+          price: 20,
+          peoples: 20,
+          limitPeoples: 53,
+          tags: ["go", "js"],
+        }}
+      />
+      <ChannelCardComponent
+        {...{
+          title: "hello",
+          rating: 4.7,
+          price: 50,
+          peoples: 20,
+          limitPeoples: 153,
+          tags: ["go", "exp", "hello world"],
+        }}
+      />
+      <ChannelCardComponent
+        {...{
+          title: "hello",
+          rating: 5.0,
+          price: 20,
+          peoples: 20,
+          limitPeoples: 53,
+          tags: ["go", "js"],
+        }}
+      />
+      <ChannelCardComponent
+        {...{
+          title: "hello",
+          rating: 3.3,
+          price: 20,
+          peoples: 20,
+          limitPeoples: 53,
+          tags: ["go", "hello javascript"],
+        }}
+      />
     </ul>
   </section>
 );

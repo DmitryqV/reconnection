b93bd2a39080bd3770397e97477946e86c3ececa
dmitryqv <vlasenko_dima@icloud.com>
2022-06-23 15:02:34 +0500

    added mongoose

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ChannelsModule } from "./channels/channels.module";
 
 @Module({
-  imports: [ChannelsModule],
+  imports: [
+    ChannelsModule,
+    MongooseModule.forRoot("")
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

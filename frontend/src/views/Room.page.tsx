afbfb7086812c7cce6457aca51017fa687c98bac
dmitryqv <vlasenko_dima@icloud.com>
2022-06-26 07:03:41 +0500

    create room styles

--- frontend/src/views/Room.page.tsx
+++ frontend/src/views/Room.page.tsx
@@ -1,7 +1,27 @@
 import React, { FC } from "react";
+import "./styles/Room.style.css";
+
 import { useParams } from "react-router-dom";
 
 export const RoomPage: FC = () => {
   const params = useParams();
-  return <>Room page {params.id}</>;
+  return (
+    <section className="room__section">
+      <ul className="room__section__wrapper">
+        <li className="room__user"></li>
+        <li className="room__user"></li>
+        <li className="room__user"></li>
+        <li className="room__user"></li>
+        <li className="room__user"></li>
+        <li className="room__user"></li>
+        <li className="room__user"></li>
+        <li className="room__user"></li>
+        <li className="room__user"></li>
+        <li className="room__user"></li>
+        <li className="room__user"></li>
+        <li className="room__user"></li>
+      </ul>
+      <ul className="room__section__chat"></ul>
+    </section>
+  );
 };

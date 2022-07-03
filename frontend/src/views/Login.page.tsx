9ce720dc5a68a638edbd98a59e6e7d37c5111b33
dmitryqv <vlasenko_dima@icloud.com>
2022-07-03 20:02:27 +0500

    added basic validation for inputs

--- frontend/src/views/Login.page.tsx
+++ frontend/src/views/Login.page.tsx
@@ -2,14 +2,28 @@ import React, { FC, useState, ChangeEvent } from "react";
 import "./styles/Login.style.css";
 
 export const LoginPage: FC = () => {
+  const regexp = process.env.NODE_ENV === "production" ? /^([a-z0-9]{10,})$/ : /^([a-z0-9]{1,})$/;
+
   const [state, setState] = useState({
     login: null,
     password: null,
   });
 
+  const validateLogin = (): boolean => {
+    return regexp.test(state.login);
+  };
+
+  const validatePassword = (): boolean => {
+    return regexp.test(state.password)
+  };
+
+  const validation = () => {
+    [validateLogin(), validatePassword()].every((el: boolean) => el === true) && (window.location.href = "/m");
+  };
+
   return (
     <section className="login__section">
-      <form className="login__form">
+      <section className="login__form">
         <p className="login__title">Авторизация</p>
         <input
           onInput={(e: ChangeEvent<HTMLInputElement>) =>
@@ -27,10 +41,10 @@ export const LoginPage: FC = () => {
           placeholder="Пароль"
           type="password"
         />
-        <button className="login__submit" type="submit">
+        <button className="login__submit" type="submit" onClick={validation}>
           Войти
         </button>
-      </form>
+      </section>
     </section>
   );
 };

b92c5e722608b9a4db4a44a15a2f62a671fd756d
dmitryqv <vlasenko_dima@icloud.com>
2022-07-22 18:27:54 +0500

    update

--- frontend/src/views/Room.page.tsx
+++ frontend/src/views/Room.page.tsx
@@ -3,139 +3,20 @@ import "./styles/Room.style.css";
 import user from "../assets/user.svg";
 
 export const RoomPage: FC = () => {
+  const users = [1,2,3,4,5,6];
+  console.log(users);
   return (
     <section className="room__section">
       <h2 className="room__title">Jaba Script Conference</h2>
       <section className="room__owner">
-        <div className="room__meta"></div>
+        <div className="room__meta">321</div>
       </section>
       <ul className="room__other">
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
-        <li className="room__user">
-          <img src={user} />
-        </li>
+        {users.map((_: unknown, index: number) => (
+          <li className="room__user" key={"user__"+index}>
+            <img src={user} />
+          </li>
+        ))}
       </ul>
     </section>
   );
